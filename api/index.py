b082c28f59cc64a58b92bdd8859c6b933120ca7a
Tomás Ramírez <samot.0309@gmail.com>
2025-11-01 14:29:20 +0100

    v41

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, g, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 import pytz
 
